299a24d69ecadf047155d6bd7af10be6aceb9e1d
Lonnie Wibberding <wibberding@gmail.com>
2019-05-23 13:43:44 -0700

    Initial display working

--- race.py
+++ race.py
@@ -8,30 +8,30 @@ race_start_time = 0
 
 left_lane_start_time = 0
 left_lane_end_time = 0
-left_lane_reaction_time = 0
-left_lane_total_time = 0
+left_lane_reaction_time = 0.00
+left_lane_total_time = 0.00
 
 right_lane_start_time = 0
 right_lane_end_time = 0
-right_lane_reaction_time = 0
-right_lane_total_time = 0
+right_lane_reaction_time = 0.00
+right_lane_total_time = 0.00
 
 #Light states
-stage_1_left = False
-stage_2_left = False
-yellow_1_left = False
-yellow_2_left = False
-yellow_3_left = False
-green_left = False
-red_left = False
+stage_1_left = True
+stage_2_left = True
+yellow_1_left = True
+yellow_2_left = True
+yellow_3_left = True
+green_left = True
+red_left = True
 
-stage_1_right = False
-stage_2_right = False
-yellow_1_right = False
-yellow_2_right = False
-yellow_3_right = False
-green_right = False
-red_right = False
+stage_1_right = True
+stage_2_right = True
+yellow_1_right = True
+yellow_2_right = True
+yellow_3_right = True
+green_right = True
+red_right = True
 
 #List of functions needed for the game
 
@@ -54,8 +54,12 @@ def start_race():
 pygame.init() 
   
 # define the RGB value 
-# for white colour 
-black = (0,0,0) 
+# for colors
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+yellow = (255,255,0)
   
 # assigning values to X and Y variable 
 X = 800 
@@ -85,78 +89,99 @@ while True :
 
     #Draw lights in correct state
     if stage_1_left == True:
-        pygame.draw.circle(display_surface, (255,255,0), (190,50), 30)
+        pygame.draw.circle(display_surface, white, (190,50), 30)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (190,50), 30, 2)
+        pygame.draw.circle(display_surface, white, (190,50), 30, 2)
 
     if stage_1_right == True:
-        pygame.draw.circle(display_surface, (255,255,0), (610,50), 30)
+        pygame.draw.circle(display_surface, white, (610,50), 30)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (610,50), 30, 2)
+        pygame.draw.circle(display_surface, white, (610,50), 30, 2)
 
     if stage_2_left == True:
-        pygame.draw.circle(display_surface, (255,255,0), (255,50), 30)
+        pygame.draw.circle(display_surface, white, (255,50), 30)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (255,50), 30, 2)
+        pygame.draw.circle(display_surface, white, (255,50), 30, 2)
 
     if stage_2_right == True:
-        pygame.draw.circle(display_surface, (255,255,0), (545,50), 30)
+        pygame.draw.circle(display_surface, white, (545,50), 30)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (545,50), 30, 2)
+        pygame.draw.circle(display_surface, white, (545,50), 30, 2)
 
     if yellow_1_left == True:
-        pygame.draw.circle(display_surface, (255,255,0), (345,70), 50)
+        pygame.draw.circle(display_surface, yellow, (345,70), 50)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (345,70), 50, 2)
+        pygame.draw.circle(display_surface, yellow, (345,70), 50, 2)
 
     if yellow_1_right == True:
-        pygame.draw.circle(display_surface, (255,255,0), (455,70), 50)
+        pygame.draw.circle(display_surface, yellow, (455,70), 50)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (455,70), 50, 2)
+        pygame.draw.circle(display_surface, yellow, (455,70), 50, 2)
     
     if yellow_2_left == True:
-        pygame.draw.circle(display_surface, (255,255,0), (345,175), 50)
+        pygame.draw.circle(display_surface, yellow, (345,175), 50)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (345,175), 50, 2)
+        pygame.draw.circle(display_surface, yellow, (345,175), 50, 2)
 
     if yellow_2_right == True:
-        pygame.draw.circle(display_surface, (255,255,0), (455,175), 50)
+        pygame.draw.circle(display_surface, yellow, (455,175), 50)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (455,175), 50, 2)
+        pygame.draw.circle(display_surface, yellow, (455,175), 50, 2)
 
     if yellow_3_left == True:
-        pygame.draw.circle(display_surface, (255,255,0), (345,280), 50)
+        pygame.draw.circle(display_surface, yellow, (345,280), 50)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (345,280), 50, 2)
+        pygame.draw.circle(display_surface, yellow, (345,280), 50, 2)
 
     if yellow_3_right == True:
-        pygame.draw.circle(display_surface, (255,255,0), (455,280), 50)
+        pygame.draw.circle(display_surface, yellow, (455,280), 50)
     else:
-        pygame.draw.circle(display_surface, (255,255,0), (455,280), 50, 2)
+        pygame.draw.circle(display_surface, yellow, (455,280), 50, 2)
     
     if green_left == True:
-        pygame.draw.circle(display_surface, (0,255,0), (345,385), 50)
+        pygame.draw.circle(display_surface, green, (345,385), 50)
     else:
-        pygame.draw.circle(display_surface, (0,255,0), (345,385), 50, 2)
+        pygame.draw.circle(display_surface, green, (345,385), 50, 2)
 
     if green_right == True:
-        pygame.draw.circle(display_surface, (0,255,0), (455,385), 50)
+        pygame.draw.circle(display_surface, green, (455,385), 50)
     else:
-        pygame.draw.circle(display_surface, (0,255,0), (455,385), 50, 2)
+        pygame.draw.circle(display_surface, green, (455,385), 50, 2)
     
     if red_left == True:
-        pygame.draw.circle(display_surface, (255,0,0), (345,490), 50)
+        pygame.draw.circle(display_surface, red, (345,490), 50)
     else:
-        pygame.draw.circle(display_surface, (255,0,0), (345,490), 50, 2)
+        pygame.draw.circle(display_surface, red, (345,490), 50, 2)
 
     if red_right == True:
-        pygame.draw.circle(display_surface, (255,0,0), (455,490), 50)
+        pygame.draw.circle(display_surface, red, (455,490), 50)
     else:
-        pygame.draw.circle(display_surface, (255,0,0), (455,490), 50, 2)
+        pygame.draw.circle(display_surface, red, (455,490), 50, 2)
     
     
-    
-    
+    #Draw titles
+    font = pygame.font.Font(None, 40)
+    font_big = pygame.font.Font(None, 72)
+
+    left_elapsed_title = font.render('Elasped Time', True, white)
+    display_surface.blit(left_elapsed_title, (50,300)) 
+    left_elapsed= font_big.render(str(left_lane_total_time), True, white)
+    display_surface.blit(left_elapsed, (50,350)) 
+
+    right_elapsed_title = font.render('Elasped Time', True, white)
+    display_surface.blit(right_elapsed_title, (580,300)) 
+    right_elapsed= font_big.render(str(right_lane_total_time), True, white)
+    display_surface.blit(right_elapsed, (580,350)) 
+
+    left_reaction_time_title = font.render('Reaction Time', True, white)
+    display_surface.blit(left_reaction_time_title, (50,600)) 
+    left_reaction= font_big.render(str(left_lane_reaction_time), True, white)
+    display_surface.blit(left_reaction, (50,650))  
+
+    right_reaction_time_title = font.render('Reaction Time', True, white)
+    display_surface.blit(right_reaction_time_title, (580,600))
+    right_reaction= font_big.render(str(right_lane_reaction_time), True, white)
+    display_surface.blit(right_reaction, (580,650)) 
   
     # iterate over the list of Event objects 
     # that was returned by pygame.event.get() method. 
